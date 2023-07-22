aec4918743887278b9d0833644f9bddef1d4ec8a
dcastewart <dcastewart8@gmail.com>
2023-07-22 12:07:07 -0400

    Added word model with solution checker

---
+++ browser-games-frontend/src/app/models/wordle/word.ts
@@ -0,0 +1,32 @@
+export class Word {
+    guess: Array<Letter>;
+
+    constructor(solution: string) {
+        this.guess = Array(solution.trim().length).fill(new Letter)
+    }
+
+    public checkSolution(solution: string) {
+        for(let i = 0; i < this.guess.length; i++) {
+            if(this.guess[i].letter == solution.charAt(i)){
+                this.guess[i].state = LetterState.Correct
+            } else if(solution.includes(this.guess[i].letter)) {
+                //TODO - check number of correct letters
+                this.guess[i].state = LetterState.WrongPosition
+            } else {
+                this.guess[i].state = LetterState.Incorrect
+            }
+        }
+    }
+}
+
+class Letter {
+    letter: string = ""
+    state: LetterState = LetterState.InProgress
+}
+
+enum LetterState {
+    InProgress,
+    Incorrect,
+    WrongPosition,
+    Correct
+}
