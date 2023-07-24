a4cb328ff9209a05982693b9a3631809c23d62af
dcastewart <dcastewart8@gmail.com>
2023-07-24 11:35:07 -0400

    implement submission logic

--- browser-games-frontend/src/app/components/wordle/wordle.component.ts
+++ browser-games-frontend/src/app/components/wordle/wordle.component.ts
@@ -1,27 +1,49 @@
 import { Component } from '@angular/core';
-import { Word } from 'src/app/models/wordle/word';
+import { LetterState, Word } from 'src/app/models/wordle/word';
 
 @Component({
   selector: 'app-wordle',
   templateUrl: './wordle.component.html',
-  styleUrls: ['./wordle.component.css']
+  styleUrls: ['./wordle.component.css'],
 })
 export class WordleComponent {
-  private solution: string = "words"
+  private solution: string = 'words';
 
-  guesses: Array<Word> = Array(6)
-  private currentIndex: number = 0
+  guesses: Array<Word> = Array(6);
+  private currentIndex: number = 0;
 
   ngOnInit() {
-    for(let i = 0; i < this.guesses.length; i++) {
-      this.guesses[i] = new Word(this.solution)
+    for (let i = 0; i < this.guesses.length; i++) {
+      this.guesses[i] = new Word(this.solution);
     }
-    this.guesses[0].guess[0].letter = 'w'
-    this.guesses[0].guess[1].letter = 'h'
-    this.guesses[0].guess[2].letter = 'o'
-    this.guesses[0].guess[3].letter = 'l'
-    this.guesses[0].guess[4].letter = 'e'
-    this.guesses[0].checkSolution("words")
+    this.guesses[0].guess[0].letter = 'w';
+    this.guesses[0].guess[1].letter = 'h';
+    this.guesses[0].guess[2].letter = 'o';
+    this.guesses[0].guess[3].letter = 'l';
+    this.guesses[0].guess[4].letter = 'e';
+    this.guesses[0].checkSolution('words');
   }
 
+  submitGuess() {
+    this.guesses[this.currentIndex].checkSolution(this.solution);
+    this.guesses[this.currentIndex].guess.forEach((value) => {
+      if (value.state != LetterState.Correct) {
+        this.currentIndex++;
+        if(this.currentIndex >= this.guesses.length) {
+          this.defeat()
+          return
+        }
+        return;
+      }
+    });
+    this.victory()
+  }
+
+  victory() {
+    // TODO - implement victory logic
+  }
+  
+  defeat() {
+    // TODO - implement loss logic
+  }
 }
