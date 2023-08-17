6828b586ff0403da90eee8581891c1b882e971d3
dcastewart <dcastewart8@gmail.com>
2023-08-17 15:29:41 -0400

    Add method to generate random indexes for MS

--- browser-games-frontend/src/app/models/minesweeper/mineField.ts
+++ browser-games-frontend/src/app/models/minesweeper/mineField.ts
@@ -11,4 +11,27 @@ export class MineField {
         }
 
     }
+
+    private generateLocations(width: number, height: number, mineCount: number): Array<number> {
+
+        //create array of indexes from 0 -> total tile count
+        let possibleValues = Array()
+        for(let i = 0; i < width*height; i++){
+            possibleValues.push(i)
+        }
+
+        //choose random index and swap with the last element
+        //run again ignoring the last element moved to the end until the last mineCount elements have been selected
+        for(let i = 0; i < mineCount; i++){
+            let indexToAdd = Math.random() * (possibleValues.length - i - 1)
+
+            let temp = possibleValues[indexToAdd]
+            possibleValues[indexToAdd] = possibleValues[possibleValues.length - i - 1]
+            possibleValues[possibleValues.length - i - 1] = temp
+        }
+
+        //return the last mineCount elements of array as random list
+        return possibleValues.slice(-mineCount)
+
+    }
 }
